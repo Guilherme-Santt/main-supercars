4ca37349c3af44eeba3fa59ba636bef3b0dfdeb9
Guilherme-Santt <guisant@icloud.com>
2025-04-06 10:03:15 -0300

    Estilizando primeira div do projeto

--- js/main.js
+++ js/main.js
@@ -3,14 +3,13 @@
     const listImg = document.querySelectorAll('img')
     const ativarTexto = document.querySelectorAll('section')
 
-    if(listImg.length && ativarTexto.length) {
+    if(listImg.length && ativarTexto.length) {    
         ativarTexto[0].classList.remove('textSection');
-
-    function activeTab(index) {
-        ativarTexto.forEach((section) => {
-            section.classList.add('textSection');
-        });
-        ativarTexto[index].classList.remove('textSection');
+        function activeTab(index) {
+            ativarTexto.forEach((section) => {
+                section.classList.add('textSection');
+            });
+            ativarTexto[index].classList.remove('textSection');
     }
 
     listImg.forEach((itemMenu, index) => {
